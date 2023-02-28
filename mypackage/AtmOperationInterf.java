9ff3e97fcc0d5ef24b6e287ec89ea848f9ed395b
SHUBHAM PANDIT <114135970+shubhampandit1143@users.noreply.github.com>
2023-02-28 10:53:41 +0530

    Add files via upload

---
+++ mypackage/AtmOperationInterf.java
@@ -0,0 +1,9 @@
+package mypackage;
+
+public interface AtmOperationInterf {
+	
+	    public void viewBalance();
+	    public void withdrawAmount(double withdrawAmount);
+	    public void depositAmount(double depositAmount );
+	    public  void viewMiniStatement();
+	}
